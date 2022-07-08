d3f24117dc425895db16baa03dc3e02be2474cc2
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-08 21:56:25 +0100

    player classes implemented. draw not working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,7 @@
-from platform import java_ver
 from app import app
 from flask import render_template, request, redirect
 from models.game import Game
+from models.player import Player
 
 
 @app.route("/")
@@ -9,6 +9,7 @@ def welcome():
     return render_template("index.html", title="Rock, Paper, Scissors Game")
 
 
+# 2 player in address bar
 @app.route("/<p1_choice>/<p2_choice>")
 def basic(p1_choice, p2_choice):
     return render_template(
@@ -16,27 +17,61 @@ def basic(p1_choice, p2_choice):
         p1_choice=p1_choice,
         p2_choice=p2_choice,
         title="Rock Paper Scissors Game",
-        result=Game.play(p1_choice, p2_choice),
+        result=Game.basic_play(p1_choice, p2_choice),
     )
 
 
+# vs computer in address bar
 @app.route("/computer/<p_choice>")
 def computer(p_choice):
     return render_template(
-        "computer.html",
+        "basic_computer.html",
         p_choice=p_choice,
         title="Rock, Paper, Scissors Game",
-        result=Game.play_computer(p_choice),
+        result=Game.basic_play_computer(p_choice),
     )
 
 
+# vs computer using form input
 @app.route("/play", methods=["GET", "POST"])
 def play():
     if request.method == "POST":
         player_name = request.form.get("p1-name")
         player_choice = request.form.get("p1-choice")
-        return Game.play_computer(player_choice)
-    return render_template(
-        "play.html",
-        title="Rock, Paper, Scissors Game",
-    )
+        player_1 = Player(player_name, player_choice)
+        computer_choice = Game.computer_choice()
+        computer = Player("Computer", computer_choice)
+        result = Game.play_computer(player_1, computer)
+        return render_template(
+            "winner.html",
+            result=result.name,
+            player_name=player_name,
+            player_choice=player_choice,
+            computer_name="computer",
+            computer_choice=computer_choice,
+        )
+    return render_template("v_computer.html", title="Rock, Paper, Scissors Game")
+
+
+# 2 PLayer using form input
+@app.route("/2player", methods=["GET", "POST"])
+def two_play():
+    if request.method == "POST":
+        player1_name = request.form.get("p1-name")
+        player1_choice = request.form.get("p1-choice")
+        player_1 = Player(player1_name, player1_choice)
+
+        player2_name = request.form.get("p2-name")
+        player2_choice = request.form.get("p2-choice")
+        player_2 = Player(player2_name, player2_choice)
+
+        result = Game.play_computer(player_1, player_2)
+        return render_template(
+            "winner_2play.html",
+            result=result.name,
+            player1_name=player1_name,
+            player1_choice=player1_choice,
+            player2_name=player2_name,
+            player2_choice=player2_choice,
+        )
+    return render_template("2player.html", title="Rock, Paper, Scissors Game")
--- models/game.py
+++ models/game.py
@@ -1,8 +1,13 @@
 from random import randint
+from models.player import Player
 
 
 class Game:
-    def play(player1_choice, player2_choice):
+    def __init__(self, _player_1, _player_2):
+        self.player_1 = _player_1
+        self.player_2 = _player_2
+
+    def basic_play(player1_choice, player2_choice):
         while player1_choice != player2_choice:
             if player1_choice == "rock" and player2_choice == "scissors":
                 return f"Player 1 wins by playing rock"
@@ -14,7 +19,7 @@ class Game:
                 return f"Player 2 wins"
         return f"It is a draw"
 
-    def play_computer(player_choice):
+    def basic_play_computer(player_choice):
         computer = randint(1, 3)
         if computer == 1:
             computer_choice = "rock"
@@ -33,3 +38,36 @@ class Game:
             else:
                 return f"Computer wins. Computer chose {computer_choice}"
         return f"It is a draw"
+
+    def play(player_1, player_2):
+        while player_1.choice.lower() != player_2.choice.lower():
+            if player_1.choice.lower() == "rock" and player_2.choice.lower() == "scissors":
+                return player_1
+            elif player_1.choice.lower() == "paper" and player_2.choice.lower() == "rock":
+                return player_1
+            elif player_1.choice.lower() == "scissors" and player_2.choice.lower() == "paper":
+                return player_1
+            else:
+                return player_2
+        return None
+
+    def computer_choice():
+        computer_rand = randint(1, 3)
+        if computer_rand == 1:
+            return "rock"
+        elif computer_rand == 2:
+            return "paper"
+        else:
+            return "scissors"
+
+    def play_computer(player_1, computer):
+        while player_1.choice.lower() != computer.choice.lower():
+            if player_1.choice.lower() == "rock" and computer.choice.lower() == "scissors":
+                return player_1
+            elif player_1.choice.lower() == "paper" and computer.choice.lower() == "rock":
+                return player_1
+            elif player_1.choice.lower() == "scissors" and computer.choice.lower() == "paper":
+                return player_1
+            else:
+                return computer
+        return None
